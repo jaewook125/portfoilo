f1717dc8eaac1a8bdf82acbfc2fc43dd0b07e35f
sjw0484 <sjw0484@naver.com>
2018-05-03 14:09:50 +0900

    기본 로그인 로그아웃 회원가입 프로필 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import login, logout
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-
+	path('signup', views.signup, name='signup'),
+	path('login/', login, name='login', kwargs={
+			'template_name' : 'accounts/login_form.html',
+		}),
+	path('profile/', views.profile, name='profile'),
+	path('logout/', logout, name='logout', kwargs={
+			'next_page':reverse_lazy('root'),
+		}),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy #success_url 활용 될때 순차적으로 실행하지않고 "게으르게" 실행
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+signup = CreateView.as_view(model=User, form_class=UserCreationForm,
+							template_name='accounts/signup_form.html',
+							success_url=reverse_lazy('root')) 
+							#success_url이 활용이 될때 root실행
+
+
+
+@login_required
+def profile(request):
+	return render(request, 'accounts/profile.html')
+from django.shortcuts import render
 
 # Create your views here.
